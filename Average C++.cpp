3fb57f26c87c37a4668405ce97ad0f64215adaf4
wmanel <35614798+wmanel@users.noreply.github.com>
2018-08-29 16:00:20 -0400

    Add files via upload

---
+++ Average C++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int a, b, c, sum;
+	float average;
+	cin >> a >> b >> c;
+	average = (a + b + c) / 3;
+	cout << "Average is:" << average << endl;
+	return 0;
+}
